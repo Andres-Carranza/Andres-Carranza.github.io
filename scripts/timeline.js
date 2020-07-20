ccb6b98de21f0b6384212dcaab33c59a93c4d546
Andres-Carranza <carranzamoreno9@gmail.com>
2020-07-19 20:45:12 -0700

    andres - update timeline

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -10,29 +10,81 @@ var sticky = timeline.offsetTop;
 
 // Add the sticky class to the navtab when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function updateSticky()  {
-  if (window.pageYOffset >= sticky) {
-    timline.classList.add("sticky")
+  if (window.pageYOffset + 30 >= sticky) {
+    timeline.classList.add("sticky")
   } else {
     timeline.classList.remove("sticky");
   }
 }
 
 
-window.addEventListener('scroll', function () {
-    content = document.getElementsByClassName('timeline-content')[0]
-    lineWidth = document.getElementsByClassName('ul')[0].offsetWidth
-    pos = 0;
+function changeActive (id) {
+    if (document.getElementsByClassName('active').length) 
+      document.getElementsByClassName('active')[0].classList.remove('active');
+  
+    active = document.getElementById(id);
+    if( active != null)
+      active.classList.add('active')
+  }
 
-    sections = document.getElementsByClassName('section')
+window.addEventListener('scroll', function () {
+    updateSticky()
+
+    var content = document.getElementsByClassName('timeline-content')[0]
+    var pos = -1;
+    var relPos = window.scrollY  +  window.innerHeight / 2
+    var sections = document.getElementsByClassName('section')
 
     for (var i = sections.length -1 ; i>=0; i--){
-        if (window.scrollY + window.innerHeight / 2 > sections[i].offsetTop  ) {
-          pos = i + 1;
+        if ( relPos> sections[i].offsetTop  ) {
+          pos = i;
+          changeActive(i);
           break;
         }
+        if(i == 0 )
+            changeActive('none')
     }
 
-    console.log(pos * lineWidth / 6 +'px')
+    var width = 0
+    var line_width = 0
 
-    document.getElementsByClassName('progress-bar')[0].style.width = pos * lineWidth / 6 +'px'
+    var ticks = document.querySelectorAll('._1,._2')
+
+    Array.from(ticks).forEach( function (element, index) {
+        if( index  < pos){
+            width+= element.offsetWidth
+        }
+        else if (index == pos){
+            width += element.offsetWidth / 2
+            if(index == 5){
+                line_width = ticks[index].offsetWidth /2
+            }
+        }
+       else if(index - 1 == pos){
+            if(index == 0 )
+                line_width = ticks[index].offsetWidth /2
+            else{
+                line_width = ticks[index].offsetWidth /2 + ticks[index-1].offsetWidth / 2
+            }
+        }
+    })
+
+    var secPos = [content.offsetTop]
+
+    Array.from(sections).forEach(element =>{
+        secPos.push(element.offsetTop)
+    })
+
+    secPos.push(document.body.clientHeight)
+
+    var progress = 0
+
+    progress = (relPos - secPos[pos + 1]) / (secPos[pos+2] - secPos[pos+1])
+
+    progress = Math.max(progress, 0)
+
+    var progressBar = document.getElementsByClassName('progress-bar')[0]
+
+
+    progressBar.style.width = width + line_width * progress +'px'
 })
