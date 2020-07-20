aa242018d768401c0bc8c931ee33eba9908a8835
Andres-Carranza <carranzamoreno9@gmail.com>
2020-07-19 17:58:09 -0700

    andres - update timeline

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,29 +1,38 @@
-window.addEventListener("scroll", function() {
-    var timelineContent = document.getElementsByClassName("timeline-content")[0];
-    var overviews = timelineContent.children
+var height = document.getElementsByClassName('timeline-content')[0].offsetHeight - 125 * 2
+document.getElementsByClassName('vl')[0].style.height = height+'px'
 
-    var pos = window.scrollY 
-    var current = -1;
 
-    for(i = overviews.length - 1; i >= 0; i--){
-        if ( pos >  overviews[i].offsetTop){
-            summary = overviews[i].getElementsByClassName('summary')[0]
-            if(!summary.classList.contains('visible-summary')){
-                summary.classList.add('visible-summary');
-            }
-            current = i;
-            break;
+// Get the timeline
+var timeline = document.getElementsByClassName("timeline")[0];
+
+// Get the offset position of the navtab
+var sticky = timeline.offsetTop;
+
+// Add the sticky class to the navtab when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function updateSticky()  {
+  if (window.pageYOffset >= sticky) {
+    timline.classList.add("sticky")
+  } else {
+    timeline.classList.remove("sticky");
+  }
+}
+
+
+window.addEventListener('scroll', function () {
+    content = document.getElementsByClassName('timeline-content')[0]
+    lineWidth = document.getElementsByClassName('ul')[0].offsetWidth
+    pos = 0;
+
+    sections = document.getElementsByClassName('section')
+
+    for (var i = sections.length -1 ; i>=0; i--){
+        if (window.scrollY + window.innerHeight / 2 > sections[i].offsetTop  ) {
+          pos = i + 1;
+          break;
         }
     }
 
-    for(i = 0; i < overviews.length; i++){
-        if (i != current) {
-            summary = overviews[i].getElementsByClassName('summary')[0]
-            if(summary.classList.contains('visible-summary')){
-                summary.classList.remove('visible-summary');
-            }
-        }
-    }
+    console.log(pos * lineWidth / 6 +'px')
 
-
-  });
+    document.getElementsByClassName('progress-bar')[0].style.width = pos * lineWidth / 6 +'px'
+})
