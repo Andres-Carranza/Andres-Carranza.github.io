e73d8da131b78b7721d84a8a8bce3ae2449a2f1d
Andres-Carranza <carranzamoreno9@gmail.com>
2020-07-27 16:42:11 -0700

    andres update layout

---
+++ scripts/outlook.js
@@ -0,0 +1,65 @@
+$(".passenger .up").hide();
+$(".passenger .ov").hide();
+
+$(document).ready(function(){
+    $(".passenger .header").click(function(){
+    $(".passenger .up").toggle(300);
+    $(".passenger .down").toggle(300);
+    $(".passenger .ov").toggle(500);
+    });
+  });
+
+$(".cargo .up").hide();
+$(".cargo .ov").hide();
+
+$(document).ready(function(){
+    $(".cargo .header").click(function(){
+    $(".cargo .up").toggle(300);
+    $(".cargo .down").toggle(300);
+    $(".cargo .ov").toggle(500);
+    });
+});
+
+$(".manufacturing .up").hide();
+$(".manufacturing .ov").hide();
+
+$(document).ready(function(){
+    $(".manufacturing .header").click(function(){
+    $(".manufacturing .up").toggle(300);
+    $(".manufacturing .down").toggle(300);
+    $(".manufacturing .ov").toggle(500);
+    });
+});
+
+$(".uam .up").hide();
+$(".uam .ov").hide();
+
+$(document).ready(function(){
+    $(".uam .header").click(function(){
+    $(".uam .up").toggle(300);
+    $(".uam .down").toggle(300);
+    $(".uam .ov").toggle(500);
+    });
+});
+
+$(".nas .up").hide();
+$(".nas .ov").hide();
+
+$(document).ready(function(){
+    $(".nas .header").click(function(){
+    $(".nas .up").toggle(300);
+    $(".nas .down").toggle(300);
+    $(".nas .ov").toggle(500);
+    });
+});
+
+$(".environment .up").hide();
+$(".environment .ov").hide();
+
+$(document).ready(function(){
+    $(".environment .header").click(function(){
+    $(".environment .up").toggle(300);
+    $(".environment .down").toggle(300);
+    $(".environment .ov").toggle(500);
+    });
+});
--- scripts/toggle-menu.js
+++
@@ -1,8 +0,0 @@
-//Show menu when you click toggle button
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navBarLinks = document.getElementsByClassName('navbar-links')[0];
-
-toggleButton.addEventListener('click', () => {
-  navBarLinks.classList.toggle('active');
-});
